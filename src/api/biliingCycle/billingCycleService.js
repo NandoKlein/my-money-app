52895217f9473ac250a603eb9239e13c2dd57921
Fernando klein silva <fernando.silva@qualyteam.com.br>
2021-10-12 10:18:15 -0300

    correção do backend

--- src/api/biliingCycle/billingCycleService.js
+++ src/api/biliingCycle/billingCycleService.js
@@ -2,8 +2,18 @@ const BillingCycle = require('./billingCycle')
 const errorHandler = require('../common/errorHandler')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({new: true, runValidators: true })
+BillingCycle.updateOptions({new: true, runValidators: true})
 BillingCycle.after('post', errorHandler).after('put', errorHandler)
+
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
+        }
+    })
+})
 
 BillingCycle.route('get', (req, res, next) => {
 
@@ -11,30 +21,13 @@ BillingCycle.route('get', (req, res, next) => {
 
         if(!err) {
             res.json(docs)
-
         } else {
             res.status(500).json({errors: [error]})
         }
     })
-
-})
-
-BillingCycle.route('count', (req, res, next) => {
-
-    BillingCycle.count({}, (error, value) => {
-
-        if(error) {
-            res.status(500).json({errors: [error]})
-
-        } else {
-            res.json({value})
-        }
-    })
-
 })
 
 BillingCycle.route('summary', (req, res, next) => {
-
     BillingCycle.aggregate([{ 
         $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}} 
     }, { 
